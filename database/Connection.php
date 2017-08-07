a6c6a6b174a7da4ae3cf5c6b4d8628931cb934ca
Chen Ding <183488175@qq.com>
2017-08-07 15:49:11 +0800

    配置化数据库连接

--- database/Connection.php
+++ database/Connection.php
@@ -2,10 +2,15 @@
 
 class Connection
 {
-	public static function make()
+	public static function make($config)
 	{
 		try {
-			return new PDO('mysql:host=127.0.0.1;dbname=phper', 'root');
+			return new PDO(
+				$config['connection'] . ';dbname=' . $config['dbname'], 
+				$config['username'],
+				$config['password'],
+				$config['options']
+			);
 		} catch (PDOException $e) {
 			die('不能连接到服务器！');
 		}
