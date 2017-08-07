76498479754dcebd6f9bad63d24f42c4d36cb222
Chen Ding <183488175@qq.com>
2017-08-07 13:49:01 +0800

    分离PHP与HTML于不同文件

--- index.php
+++ index.php
@@ -1,10 +1,5 @@
-<html>
-	<header>
-		<title>PHP</title>
-	</header>
-	<body>
-		<h1>Hello, <?php echo $_GET['greet'] ?></h1>
-		<h1>Hello, <?= $_GET['greet'] ?></h1>
-		<h1>Hello, <?= htmlspecialchars($_GET['greet']) ?></h1>
-	</body>
-</html>
+<?php 
+
+$greet = 'Hello, World';
+
+require "index.view.php";
---
+++ index.view.php
@@ -0,0 +1,8 @@
+<html>
+	<header>
+		<title>PHP</title>
+	</header>
+	<body>
+		<h1><?= $greet ?></h1>
+	</body>
+</html>
