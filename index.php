19e5cc5bedd9488b28947d9d1a7406304cadad96
Chen Ding <183488175@qq.com>
2017-08-07 13:43:49 +0800

    PHP在HTML中使用

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-<?php
-
-echo 'Hello, World';
+<html>
+	<header>
+		<title>PHP</title>
+	</header>
+	<body>
+		<h1>Hello, <?php echo $_GET['greet'] ?></h1>
+		<h1>Hello, <?= $_GET['greet'] ?></h1>
+		<h1>Hello, <?= htmlspecialchars($_GET['greet']) ?></h1>
+	</body>
+</html>
