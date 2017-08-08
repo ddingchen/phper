1d71f21c9a346006e1c24a2594531f523f9410cc
Chen Ding <183488175@qq.com>
2017-08-08 16:26:24 +0800

    使用Composer自动加载类文件

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,10 +1,7 @@
 <?php 
 
 $config = require 'config.php';
-require 'functions.php';
-require 'database/Connection.php';
-require 'database/QueryBuilder.php';
-require 'Router.php';
+require 'vendor/autoload.php';
 
 $router = Router::load('routes.php');
 
